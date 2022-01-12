4a4c1d60d574605f645b1ed82e472990d12b632d
Shankar4919 <itsme.shankar8@gmail.com>
2022-01-12 18:19:19 +0100

    updates and logout

--- gearUp_backend_nodejs/src/Routes/user.routes.js
+++ gearUp_backend_nodejs/src/Routes/user.routes.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { addNewUser, changeFotoProfile, updateInformationUser, updateStreetAddress} = require('../Controller/UserController');
+const { addNewUser, logout, getUserById,changeFotoProfile, updateInformationUser, updateStreetAddress } = require('../Controller/UserController');
 const { uploadsProfile } = require('../Helpers/Multer');
 const { validateToken }  = require('../Middlewares/ValidateToken');
 
@@ -8,5 +8,8 @@ const router = Router();
     router.post('/user/add-new-user', addNewUser);
     router.get('/user/get-user-by-id', validateToken, getUserById);
     router.put('/user/update-picture-profile', [ validateToken, uploadsProfile.single('image') ], changeFotoProfile );
+    router.put('/user/update-information-user', validateToken, updateInformationUser);
+    router.put('/user/update-street-address', validateToken, updateStreetAddress);
+    router.get('/logout', logout);
 
 module.exports = router;
